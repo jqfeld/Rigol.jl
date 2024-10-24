725042b3bbc8f561098b502d45139da063f40b4c
Documenter.jl <documenter@juliadocs.github.io>
2024-10-24 05:44:36 +0000

    build based on e203921

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Rigol","category":"page"},{"location":"#Rigol","page":"Home","title":"Rigol","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Rigol.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Rigol]","category":"page"}]
+}
